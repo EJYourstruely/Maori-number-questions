4cee03e6a1d9a1ddf3f7884025f51d615740e859
EJYourstruely <taringe@middleton.school.nz>
2023-05-02 09:47:17 +1200

    sca

---
+++ Greetings_V1.py
@@ -0,0 +1,9 @@
+"""Greeting_V1 will write what the code is about and how to do the quiz"""
+
+#greetings and explaining how it works
+print("Welcome to my Māori number quiz. Here you will learn the mario numbers from 1-10"
+      "it will be not to difficult")
+#explain how the rules work and how to get points
+print("To play the Māori Quiz you need to read what the question they asked was and write the correct answer"
+      "if it is the correct answer you will gain 1 point and move up however if you dont choose the write answer"
+      "you will not gain a point but lose ")
